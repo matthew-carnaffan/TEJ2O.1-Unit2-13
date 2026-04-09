9a8645b477fa1e4f346db624c250c19345d13b80
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-04-09 13:08:25 -0400

    filled in info and made a prototype code

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matthew Carnaffan
+ * Created on: Apr 2026
+ * This program counts down from 4
 */
 
-basic.showString('Hello, World!')
+//Variable creation
+let myStrip: neopixel.Strip = null
+myStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+let strip: number
+strip = 4
+let range = myStrip.range(0, 4)
+
+//clean up
+basic.showIcon(IconNames.Happy)
+myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
+myStrip.show()
+
+//timer
+input.onButtonPressed(Button.A, function() {
+    while (strip >= 0) {
+        myStrip.showColor(neopixel.colors(NeoPixelColors.Black))
+        let range = myStrip.range(strip, -1)
+        myStrip.show()
+        basic.pause(1000)
+        strip = strip - 1
+    }
+})
